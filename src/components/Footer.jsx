6f27ff8ce0555a8219986437899c22dbfef1cabd
4lb4t0R <h4pt4sn7trir@gmail.com>
2022-10-18 10:46:12 +0200

    mise en place squelette footer et nav, les params + CSS puis créa layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import logo from '../assets/LOGO.png'
+
+function Footer (){
+return(
+    <div>
+        <a href={logo}/>
+        <p></p>
+    </div>
+)
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import logo from '../assets/LOGO.png'
+
+function Header () {
+return(
+    <div>
+        <a href={logo}/>
+    <nav>
+        <ul>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
+    </div>
+)
+}
+
+export default Header
