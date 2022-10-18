791b3c09a095ee3c983d46e1f3bbe02ac4e95b9b
4lb4t0R <h4pt4sn7trir@gmail.com>
2022-10-18 15:28:55 +0200

    footer full param et ok !

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import style from '../styles/Footer.module.css'
 function Footer (){
 return(
     <div className={style.footinit}>
-        <img src={logo} alt='Logo de la société'/>
-        <p></p>
+        <img src={logo} className={style.logo} alt='Logo de la société'/>
+        <p className={style.copyrights}>© 2020 Kasa. All rights reserved</p>
     </div>
 )
 }
