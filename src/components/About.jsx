622c79adbf5a7670afd151025dbc11958f7b9d1e
4lb4t0R <h4pt4sn7trir@gmail.com>
2022-10-19 10:43:05 +0200

    mise en place style déroulant + onglet ouvert

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,25 +5,25 @@ function About() {
   return (
     <div>
       <img src={fond} className={style.fond} alt="forêt apaisante" />
-      <p className={style.init}>Fiabilité</p>
+      <p className={style.déroulant}>Fiabilité</p>
       <p className={style.descriptif}>
         Les annonces postées sur Kasa garantissent une fiabilité totale. Les
         photos sont conformes aux logements, et toutes les informations sont
         régulièrement vérifiées par nos équipes.
       </p>
-      <p className={style.init}>Respect</p>
+      <p className={style.déroulant}>Respect</p>
       <p className={style.descriptif}>
         La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
         comportement discriminatoire ou de perturbation du voisinage entraînera
         une exclusion de notre plateforme.
       </p>
-      <p className={style.init}>Service</p>
+      <p className={style.déroulant}>Service</p>
       <p className={style.descriptif}>
         Nos équipes se tiennent à votre disposition pour vous fournir une
         expérience parfaite. N'hésitez pas à nous contacter si vous avez la
         moindre question.
       </p>
-      <p className={style.init}>Sécurité</p>
+      <p className={style.déroulant}>Sécurité</p>
       <p className={style.descriptif}>
         La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour
         les voyageurs, chaque logement correspond aux critères de sécurité
