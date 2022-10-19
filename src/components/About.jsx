a9a2c558f3f565760b007c096b2f7c01c904e4e5
4lb4t0R <h4pt4sn7trir@gmail.com>
2022-10-19 16:18:10 +0200

    je deviens maboul, dernier tuto mais sur accordion cette fois sinon nsm gros CSS des familles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,36 +1,50 @@
 import style from "../styles/About.module.css";
 import fond from "../assets/about.png";
+import React, { useState } from "react";
 
 function About() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <section className={style.propreté}>
       <img src={fond} className={style.fond} alt="forêt apaisante" />
-      <div className={style.déroulant}>
+
+      <button className={style.déroulant} onClick={() => setIsOpen(!isOpen)}>
         Fiabilité
+      </button>
+      {isOpen && (
         <p className={style.descriptif}>
           Les annonces postées sur Kasa garantissent une fiabilité totale. Les
           photos sont conformes aux logements, et toutes les informations sont
           régulièrement vérifiées par nos équipes.
         </p>
-      </div>
-      <div className={style.déroulant}>
+      )}
+
+      <button className={style.déroulant} onClick={() => setIsOpen(!isOpen)}>
         Respect
+      </button>
+      {isOpen && (
         <p className={style.descriptif}>
           La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
           comportement discriminatoire ou de perturbation du voisinage
           entraînera une exclusion de notre plateforme.
         </p>
-      </div>
-      <div className={style.déroulant}>
+      )}
+
+      <button className={style.déroulant} onClick={() => setIsOpen(!isOpen)}>
         Service
+      </button>
+      {isOpen && (
         <p className={style.descriptif}>
           Nos équipes se tiennent à votre disposition pour vous fournir une
           expérience parfaite. N'hésitez pas à nous contacter si vous avez la
           moindre question.
         </p>
-      </div>
-      <div className={style.déroulant}>
+      )}
+
+      <button className={style.déroulant} onClick={() => setIsOpen(!isOpen)}>
         Sécurité
+      </button>
+      {isOpen && (
         <p className={style.descriptif}>
           La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
           pour les voyageurs, chaque logement correspond aux critères de
@@ -39,7 +53,7 @@ function About() {
           standards sont bien respectés. Nous organisons également des ateliers
           sur la sécurité domestique pour nos hôtes.
         </p>
-      </div>
+      )}
     </section>
   );
 }
