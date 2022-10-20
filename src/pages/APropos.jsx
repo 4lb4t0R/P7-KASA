20fa63090930e6d9d045259e6f02e4ed191df1fa
4lb4t0R <h4pt4sn7trir@gmail.com>
2022-10-20 11:01:23 +0200

    Synchro Shoukan <3

---
+++ src/pages/APropos.jsx
@@ -0,0 +1,13 @@
+import About from "../components/About";
+import Banner from "../components/About_Banner";
+import Layout from "./Layout";
+function APropos() {
+  return (
+    <Layout>
+      <Banner />
+      <About />
+    </Layout>
+  );
+}
+
+export default APropos;
