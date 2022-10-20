8220a34f35884a9767f5ee9abbad220a095f6e18
4lb4t0R <h4pt4sn7trir@gmail.com>
2022-10-20 13:43:07 +0200

    en mémoire du papa de Yugioh, mes commits seront des refs à ce chef d'oeuvre (mais y aura aussi des vrais commits tqt moi du futur)

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,15 +1,18 @@
-/*import Erreur from "../components/Erreur";
-import Header from "..components/Header";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-<Header>
-      <Erreur />
-    </Header>
-*/
-
-import About from "../components/About";
+import Error from "./pages/Error";
+import APropos from "./pages/APropos";
 
 function App() {
-  return <About />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/APropos" element={<APropos />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
