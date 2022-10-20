8be217468c13feeeca4545fc02d565a515173fd5
4lb4t0R <h4pt4sn7trir@gmail.com>
2022-10-20 10:55:24 +0200

    finalisation page a propos, j'ai perdu trop de temps dessus, quand finie, init du router

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,34 +1,48 @@
-import style from "../styles/About.module.css";
-import fond from "../assets/about.png";
-import data from "../datas/about";
+import React from "react";
 import { useState } from "react";
+import style from "../styles/About.module.css";
+import vector from "../assets/Vector.svg";
+import { textsList } from "../datas/about";
 
-function About() {
-  const [selected, setSelected] = useState(null);
-  const trigger = (i) => {
-    if (selected === i) {
-      return setSelected(null);
+const Collapse = () => {
+  const [isOpen, setIsOpen] = useState(-1);
+
+  function toggleCollapse(index) {
+    if (index === isOpen) {
+      setIsOpen(-1);
+      return;
     }
-
-    setSelected(i);
-  };
-
+    setIsOpen(index);
+  }
   return (
-    <section className={style.propreté}>
-      <img src={fond} className={style.fond} alt="forêt apaisante" />
-
-      {data.map((item, i) => (
-        <div>
-          <div className={style.déroulant} onClick={() => trigger(i)}>
-            <h5>{item.catégorie}</h5>
+    <div>
+      {textsList.map((propos, index) => (
+        <div key={index}>
+          <div className={style.collapse} onClick={() => toggleCollapse(index)}>
+            <h2 className={style.collapsetext}>{propos.title}</h2>
+            <div>
+              <img
+                src={vector}
+                alt="flèche"
+                className={
+                  isOpen === index
+                    ? style.collapsearrow && style.collapsearrowturn
+                    : style.collapsearrow
+                }
+              />
+            </div>
           </div>
-          <div className={selected === i ? "descriptif tada" : "descriptif"}>
-            {item.descriptif}
+          <div
+            className={
+              isOpen === index ? style.collapsedescription : style.displaynone
+            }
+          >
+            <p>{propos.description}</p>
           </div>
         </div>
       ))}
-    </section>
+    </div>
   );
-}
+};
 
-export default About;
+export default Collapse;
---
+++ src/pages/Error.jsx
@@ -0,0 +1,12 @@
+import Erreur from "../components/Erreur";
+import Layout from "./Layout";
+
+function Error() {
+  return (
+    <Layout>
+      <Erreur />
+    </Layout>
+  );
+}
+
+export default Error;
