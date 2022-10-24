c9db39b2c8e41b382e526dbd94b889df15ff9ace
4lb4t0R <h4pt4sn7trir@gmail.com>
2022-10-24 10:57:59 +0200

    créa component de nota, page logement à créer et finito, restera que du ménage et des retouches CSS oklm

--- src/components/Notation.jsx
+++ src/components/Notation.jsx
@@ -1,5 +1,24 @@
-import style from "../styles/Notation.module.css";
+import React, { useState } from "react";
+import { Rating } from "react-simple-star-rating";
+import greystar from "../assets/star_rate-24-grey";
+import redstar from "../assets/star_rate-24-red";
 
-const Notation = () => {};
+export default function MyComponent() {
+  const [rating, setRating] = useState(0);
 
-export default Notation;
+  // Catch Rating value
+  const handleRating = (rate) => {
+    setRating(rate);
+  };
+
+  return (
+    <div className="App">
+      <Rating
+        ratingValue={rating}
+        fillIcon={redstar}
+        emptyIcon={greystar}
+        onClick={handleRating}
+      />
+    </div>
+  );
+}
