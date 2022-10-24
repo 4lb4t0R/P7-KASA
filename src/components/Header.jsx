c894fd53dc78632aff61b9a97aee8af0f1e9d2ce
4lb4t0R <h4pt4sn7trir@gmail.com>
2022-10-24 15:50:35 +0200

    upgrade du header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import logo from "../assets/LOGO.png";
 import style from "../styles/Header.module.css";
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className={style.headinit}>
       <img src={logo} className={style.logo} alt="Logo de la société" />
-      <p className={style.choix}>Accueil</p>
-      <p className={style.choix}>À propos</p>
+      <Link className={style.choix} to="/Home">
+        Accueil
+      </Link>
+      <Link className={style.choix} to="/A_Propos">
+        À propos
+      </Link>
     </div>
   );
 }
