01f2ce67e83ffce84a571d942d6a9d1f9d6ffddf
4lb4t0R <h4pt4sn7trir@gmail.com>
2022-10-24 15:24:18 +0200

    A CHECKER : CSS DE ABOUT ET DE LA NOTATION, SINON GG

--- src/pages/Page_Logement.jsx
+++ src/pages/Page_Logement.jsx
@@ -1,5 +1,62 @@
-const Page_Logement = () => {
-  return <p>envie de crever</p>;
+import React from "react";
+import Layout from "../pages/Layout";
+import StarRating from "../components/Notation";
+import Collapse from "../components/About";
+import { useAppartment } from "../components/Hook_Logement";
+import "../datas/logements";
+import style from "../styles/Page_Logement.module.css";
+import Slideshow from "../components/Slideshow.jsx";
+
+const FicheLogement = () => {
+  const h = useAppartment();
+  return (
+    <Layout>
+      <Slideshow pictures={h.appartment ? h.appartment.pictures : []} />
+      <div>
+        <div className={style.enteteflex}>
+          <div>
+            <div className={style.title}>
+              <div>
+                <h1 className={style.title}>{h.appartment?.title}</h1>
+                <p className={style.location}>{h.appartment?.location}</p>
+                <ul className={style.ulmargin.tags}>
+                  {h.appartment?.tags &&
+                    h.appartment?.tags.map((tag) => (
+                      <li key={tag} className="style.tag">
+                        {tag}{" "}
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className={style.hostrateflex}>
+            <div className={style.host}>
+              <p className={style.hostname}> {h.appartment?.host.name}</p>
+              <img
+                className={style.hostpicture}
+                src={h.appartment?.host.picture}
+                alt="profil de l'hôte"
+              />
+            </div>
+            <div>
+              <StarRating
+                score={h.appartment ? parseInt(h.appartment.rating) : 0}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={style.descriptionlogement}>
+          <div className={style.collapselogement}>
+            <Collapse title="Description" content={h.appartment?.description} />
+          </div>
+          <div className={style.collapselogement}>
+            <Collapse title="Équipements" contents={h.appartment?.equipments} />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
-export default Page_Logement;
+export default FicheLogement;
